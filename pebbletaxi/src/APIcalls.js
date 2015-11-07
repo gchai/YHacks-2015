bb7196b308f1ba8e117bb5f7d47809f0661e2920
flak153 <flakgun153@gmail.com>
2015-11-07 18:41:18 -0500

    added js

---
+++ pebbletaxi/src/APIcalls.js
@@ -0,0 +1,3 @@
+Pebble.addEventListener('ready', function(e) {
+  console.log('JavaScript app ready and running!');
+});
--- pebbletaxi/src/main.c
+++ pebbletaxi/src/main.c
@@ -4,14 +4,26 @@ static Window *s_main_window;
 static TextLayer *s_output_layer;
 
 static DictationSession *s_dictation_session;
-static char s_last_text[512];
-
+static char s_last_text[256];
+static char s_buffer[256];
+enum {
+  KEY_CURCOR = 1,
+  KEY_TARGET = 2,
+};
 /******************************* Dictation API ********************************/
 
 static void dictation_session_callback(DictationSession *session, DictationSessionStatus status, 
                                        char *transcription, void *context) {
   if(status == DictationSessionStatusSuccess) {
     // Display the dictated text
+    text_layer_set_text(s_output_layer, s_last_text);
+    DictionaryIterator *iterator;
+    app_message_outbox_begin(&iterator);
+    uint8_t buffer[256];
+    dict_write_begin(iterator, buffer, sizeof(buffer));
+    dict_write_cstring(iterator, KEY_TARGET, transcription);// Write the CString:dict_write_cstring(&iter, SOME_STRING_KEY, string);// End:
+    const uint32_t final_size = dict_write_end(iterator);
+    
     snprintf(s_last_text, sizeof(s_last_text), "Transcription:\n\n%s", transcription);
     text_layer_set_text(s_output_layer, s_last_text);
   } else {
@@ -28,6 +40,25 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   // Start voice dictation UI
   dictation_session_start(s_dictation_session);
 }
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Get the first pair
+  Tuple *data = dict_find(iterator, KEY_CURCOR);
+  if (data) {
+    snprintf(s_buffer, sizeof(s_buffer), "Received '%s'", data->value->cstring);
+  }
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
 
 static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
@@ -38,7 +69,7 @@ static void window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   s_output_layer = text_layer_create(GRect(bounds.origin.x, (bounds.size.h - 24) / 2, bounds.size.w, bounds.size.h));
-  text_layer_set_text(s_output_layer, "Press Select to get input!");
+  text_layer_set_text(s_output_layer, "Press Select to dictate a destination");
   text_layer_set_text_alignment(s_output_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
 }
@@ -50,6 +81,11 @@ static void window_unload(Window *window) {
 static void init() {
   s_main_window = window_create();
   window_set_click_config_provider(s_main_window, click_config_provider);
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  app_message_open(64, 64);
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
